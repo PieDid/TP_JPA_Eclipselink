36e07765e37f69e0dadb15b2aaad2b2ac7515b51
PieDid <pierrickflaux@sfr.fr>
2020-04-10 18:06:46 +0200

    5 : implementation des entité Cargaison, CargaisonAerienne, CargaisonRoutiere

--- TP_Gestion_Transport_Maritime/src/com/intiformation/tpjpa/entity/CargaisonRoutiere.java
+++ TP_Gestion_Transport_Maritime/src/com/intiformation/tpjpa/entity/CargaisonRoutiere.java
@@ -1,5 +1,57 @@
 package com.intiformation.tpjpa.entity;
 
-public class CargaisonRoutiere {
+import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+/**
+ * Classe entité.
+ * Classe fille de Cargaison.
+ * @author IN-DF-028
+ *
+ */
+
+@Entity(name = "routiere")
+@DiscriminatorValue("Routiere")
+public class CargaisonRoutiere extends Cargaison{
+
+	/*_________________ props ________________*/
+	
+	@Column(name = "temperature_conservation")
+	private double temperatureConservation;
+	
+	/*_________________ ctors ________________*/
+	
+	/**
+	 * ctor vide
+	 */
+	public CargaisonRoutiere() {
+		super();
+	}
+
+	public CargaisonRoutiere(String nomCargaison, double distance, Date dateLivraison, double temperatureConservation) {
+		super(nomCargaison, distance, dateLivraison);
+		this.temperatureConservation = temperatureConservation;
+	}
+
+	/*_________________ meths ________________*/
+
+	/*__________________ G/S _________________*/
+	
+	/**
+	 * @return the temperatureConservation
+	 */
+	public double getTemperatureConservation() {
+		return temperatureConservation;
+	}
+
+	/**
+	 * @param temperatureConservation the temperatureConservation to set
+	 */
+	public void setTemperatureConservation(double temperatureConservation) {
+		this.temperatureConservation = temperatureConservation;
+	}
+	
 }
