e129019b0cf8d2e1667d28cc23b132cfe3044bb1
Cheng Jun <d.cheng@live.fr>
2020-04-10 17:13:40 +0200

    4. Ajout de la classe AppTestMarchandise

--- TP_Gestion_Transport_Maritime/src/com/intiformation/tpjpa/entity/Marchandise.java
+++ TP_Gestion_Transport_Maritime/src/com/intiformation/tpjpa/entity/Marchandise.java
@@ -1,5 +1,18 @@
 package com.intiformation.tpjpa.entity;
 
-public class Marchandise {
+import java.io.Serializable;
 
-}
+import javax.persistence.Entity;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+@Entity(name="marchandise")
+@Table(name="marchandise")
+public class Marchandise implements Serializable {
+	
+	/* Propriétés */
+	
+	private Long;
+
+}//end class
---
+++ TP_Gestion_Transport_Maritime/src/com/intiformation/tpjpa/test/AppTestGestionMarchandise.java
@@ -0,0 +1,10 @@
+package com.intiformation.tpjpa.test;
+
+public class AppTestGestionMarchandise {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
